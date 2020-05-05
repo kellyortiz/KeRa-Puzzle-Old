4f62b395dee35ee5c7dab30d26ba46d72b6f46fd
Kelly Ortiz <31560921@mackenzista.com.br>
2020-05-05 15:28:27 -0300

    Colocação da segunda versão do jogo

---
+++ Puzzle-Version2/Assets/Script/Manager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class Manager : MonoBehaviour
+{
+    public GameObject win, gameover;
+    public Image Image1, Image2;
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    public int correct_index;
+    
+
+    void Start()
+    {
+        win.gameObject.SetActive(false);
+        gameover.gameObject.SetActive(false);
+        correct_index = Random.Range(0, audioClips.Length);
+        audioSource.clip = audioClips[correct_index];
+        Debug.Log(correct_index);
+        
+    }
+
+    public void PlayClicked()
+    {
+        audioSource.Play();
+    }
+    
+    public void ButtonClicked(int id)
+    {
+       
+        if (id == correct_index)
+        {          
+            //Elimina a possibilidade de clicar nas imagens e som
+            Image1.enabled = false;
+            Image2.enabled = false;
+            audioSource.enabled = false;          
+            
+            win.gameObject.SetActive(true);
+            Debug.Log("Certa");
+        }
+            
+        
+        else
+        {
+            Image1.enabled = false;
+            Image2.enabled = false;
+            audioSource.enabled = false;
+            
+            gameover.gameObject.SetActive(true);
+            Debug.Log("Errado");
+        }
+    }
+
+}
---
+++ Puzzle-Version2/Assets/Script/ManagerMeet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ManagerMeet : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public AudioClip[] audioClips;
+    public int correct_index;
+
+
+    void Start()
+    {
+        correct_index = Random.Range(0, audioClips.Length);
+        audioSource.clip = audioClips[correct_index];
+        Debug.Log(correct_index);
+
+    }
+
+    public void PlayClicked()
+    {
+        audioSource.Play();
+    }
+
+}
